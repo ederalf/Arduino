8f30c4c9994104fb3b6dac726e4010e0a7651abc
Eder Ariza <eder.doctor.engineer@gmail.com>
2020-09-21 21:14:29 -0500

    Encabezado

--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
+
+/*-------------------------------------------------------------------------------
+  Name:        module1
+  Purpose:
+  Author:      Eder Ariza
+  Created:     02/08/2020
+  Copyright:   (c) Eder Ariza 2020
+  Licence:     <your licence>
+ -------------------------------------------------------------------------------
+ "!/usr/bin/python"
+ "-*- coding: utf-8 -*-""
+*/
+
 #include <iostream>
 
 using namespace std;
